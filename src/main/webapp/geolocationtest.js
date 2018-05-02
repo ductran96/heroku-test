9bfc5f8a18c0ccb01a356acf9d796a59bcdf99ff
Unknown <joel.vornanen2@gmail.com>
2018-05-02 10:50:05 +0300

    added front stuff

---
+++ src/main/webapp/geolocationtest.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let x = document.querySelector("demo");
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    }
+});
